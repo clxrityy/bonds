ef0190502bd4b0b3b3eebdbd7b4dbe9e523d45ab
clxrityy <clxrityy@users.noreply.github.com>
2026-04-15 22:47:41 +0000

    deploy: 2581374e7cc0927ab21c9b611124dac4985c02c6

---
+++ versions/cli-v0.1.0/api/trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["bonds_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"bonds_core/bond/struct.Bond.html\" title=\"struct bonds_core::bond::Bond\">Bond</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[284]}
