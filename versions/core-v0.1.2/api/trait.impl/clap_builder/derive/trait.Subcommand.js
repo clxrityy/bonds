9b0233aceb2a3e0e360c9e8b98e271758bd8b4a2
clxrityy <clxrityy@users.noreply.github.com>
2026-04-23 19:13:55 +0000

    deploy: 9ae9cde970c204d5983a4cc354b1d9fa1cb30796

---
+++ versions/core-v0.1.2/api/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["bonds_cli",[["impl Subcommand for <a class=\"enum\" href=\"bonds_cli/args/enum.Commands.html\" title=\"enum bonds_cli::args::Commands\">Commands</a>",0],["impl Subcommand for <a class=\"enum\" href=\"bonds_cli/args/enum.ConfigAction.html\" title=\"enum bonds_cli::args::ConfigAction\">ConfigAction</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[309]}
---
+++ versions/core-v0.1.2/api/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["bonds_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"bonds_core/error/enum.BondError.html\" title=\"enum bonds_core::error::BondError\">BondError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[287]}
