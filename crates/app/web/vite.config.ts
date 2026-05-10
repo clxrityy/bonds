f9fa03a69724930563aac20d67146a269da1f217
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-05-09 18:00:28 -0700

    feat(app): add react

--- crates/app/web/vite.config.ts
+++ crates/app/web/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	root: resolve(__dirname),
+	plugins: [react(), tailwindcss()],
 	clearScreen: false,
 	server: {
 		port: 1420,
